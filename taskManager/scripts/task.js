2204f5ec3b26bf8f3c852bdf8d29285ba69b1319
chucktagg83 <bgtagg@gmail.com>
2026-03-02 17:58:08 -0700

    Assignment 2 Updates

--- taskManager/scripts/task.js
+++ taskManager/scripts/task.js
@@ -1,10 +1,10 @@
 class Task{
-    constructor(title,description,color,data,status,budget){
+    constructor(title,description,color,date,status,budget){
         this.title=title;
         this.description=description;
         this.color=color;
         this.date=date;
         this.status=status;
-        this.budget=budget
+        this.budget=budget;
     }
 }
