889dbfd65d2777beda9b0f4fe7b59a4938ed79c9
chucktagg83 <bgtagg@gmail.com>
2026-03-07 20:43:32 -0700

    Added delete button, and Final Report

--- taskManager/scripts/app.js
+++ taskManager/scripts/app.js
@@ -15,42 +15,53 @@ function saveTask(){
     // create a new task object
     const task = new Task(title,desc,color,date,status,budget);
     console.log(task);
-    displayTask(task);  
     //Send to server
-
+    
     $.ajax({
-        type:"POST",
+        type:"POST",//HTTP Verb: Create
         url:API,
         data:JSON.stringify(task),
         contentType:"application/json",
         success: function(created){
-            displayTask(task)
+            displayTask(created); 
             console.log(created);
-            
         },
         error: function(err){
             console.log(err);
         }
-
-    })
+     })  
 }
+
+// minichallenge 
+// use put method to update one of the existing entrys
+// tip: you must use the ID - url: API/# -- 
+// https://106api-b0bnggbsgnezbzcz.westus3-01.azurewebsites.net/api/tasks/#
+// modify the entry with the ID=1, using a title that says "Hello my name is - your name"
+
+
 
 function displayTask(task){
     let syntax = `
-    <div class="task" style="border-color:${task.color}">
+    <div class="task" id="${task.id}" style="border-color:${task.color}">
       <div class="info">
         <h4>${task.title}</h4>
-        <p>${task.desc}</p>
+        <p>${task.description}</p>
       </div>
       <label class="status">${task.status}</label>
       <div class="date-budget">
         <label>Due: ${task.date}</label>
-        <label>Budget: $${task.budget}</label>
-      </div>
+        <label>Budget: ${task.budget}</label>
+        
+        <div>
+        <button class="btn-delete">Delete</button>
+        </div>
+
+        </div>
     </div>`;
     
   // Inject the new HTML into the DOM Tree
   $(".list").append(syntax);
+  
 
 }
 
@@ -62,7 +73,7 @@ function loadTasks(){
     $.ajax({
         type:"GET",// HTTP method - READ
         url: API,
-        datatype: "json", //Expected formal
+        dataType: "json",//Expected formal
         success: function(data){
             console.log("Data received", data);
         },
@@ -72,6 +83,39 @@ function loadTasks(){
     })
 }
 
+function deleteTask(){
+    console.log("Delete task");
+    //1.Context: "this" is the specific button that was clicked
+    let btn = $(this);
+    //2. Find the parent div 
+    let taskElement = btn.parents(".task");
+    // btn.closest
+    //3. Extraction: get the ID we saved in the HTML
+    let id = taskElement.attr("id");
+    console.log("Element to delete", id);
+    if (!id){
+        console.log("No id found");
+        return;
+    }    
+    //4. server communication
+    $.ajax(
+        {
+            type:"DELETE",//HTTP: Verb Delete
+            url: API + "/" + id,
+            success:function(){
+                //remove the element
+                taskElement.fadeOut(500,function(){
+                    $(this).remove();//remove from the DOM 
+                });
+            },
+            error:function(err)
+            {
+                console.log(err);
+            }
+        }
+    )
+
+}
 
 function init ()
 {
@@ -79,24 +123,13 @@ function init ()
     console.log("Hello this is the DOM");
     // hook events
     $("#btnSave").click(saveTask);
+    // $(".btn-delete").click();
+    $(".list").on("click",".btn-delete", deleteTask);
     // load data from the server
     loadTasks();
+
+
 }
 // force to my logic to run the html and css first - and when they finish the logic 
 // will be executed
 window.onload = init;
-
-function range(){
-    $.ajax({
-        type:"PUT",
-        url:API + "/4",
-        data: JSON.stringify({title: "Hello my name is Chuck!"}),
-        contentType: "application/json",
-        success: function(updated){
-            console.log(updated);
-        },
-        error: function(error){
-            console.log(error);
-        }
-    })
-}
--- taskManager/scripts/task.js
+++ taskManager/scripts/task.js
@@ -1,10 +1,13 @@
 class Task{
     constructor(title,description,color,date,status,budget){
-        this.title=title;
-        this.description=description;
-        this.color=color;
-        this.date=date;
-        this.status=status;
-        this.budget=budget;
+        this.title = title
+        this.desc = description
+        this.color = color
+        this.date = date
+        this.status = status
+        this.budget = budget
     }
 }
+
+let myTask = new Task("Buy Groceries", "Milk and Eggs", "#ff0000", "2026-02-28", "New", 50);
+myTask
